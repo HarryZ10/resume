69d1ee1a2c11895e20c5f4817be58a320cc3c815
Harry <harryzhu45@gmail.com>
2024-06-19 16:50:11 -0700

    feat: update resume

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # convert latex docs to pdf then preview it
-pdflatex main.tex && mv main.pdf Zhu_Harry_Resume.pdf && open Zhu_Harry_Resume.pdf
-
+pdflatex main.tex && mv main.pdf Zhu_Harry_Resume.pdf && open -a Preview Zhu_Harry_Resume.pdf
 # clean up1
 rm *.aux
 rm *.out
